ec2b68645579f9ab9b6561851b9d4dcc1f752745
DrGraf93 <abdulla@alfadigital.az>
2026-08-13 21:14:59 +0400

    13.08.26 21-14

--- config/config.php
+++ config/config.php
@@ -3,3 +3,6 @@ declare(strict_types=1);
 
 define('THESPORTSDB_API_KEY', '123');
 define('THESPORTSDB_BASE_URL', 'https://www.thesportsdb.com/api/v1/json/');
+
+define('API_FOOTBALL_KEY', '7b907b702c4883fcf00db3b2669ee571');
+define('API_FOOTBALL_BASE_URL', 'v3.football.api-sports.io');
